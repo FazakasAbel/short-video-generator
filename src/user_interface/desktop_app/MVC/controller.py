9dd3cc608d84661e8eafbed7156098af4dd7425f
FazakasAbel <fazakas.abel@gmail.com>
2024-05-22 12:30:22 -0400

    took a deep breath

--- src/user_interface/desktop_app/MVC/controller.py
+++
@@ -1,9 +0,0 @@
-class Controller:
-    def __init__(self, model, view):
-        self.model = model
-        self.view = view
-
-    def handle_button_click(self, button_index):
-        words = ["Word 1", "Word 2", "Word 3", "Word 4", "Word 5"]
-        self.model.set_text(words[button_index])
-        self.view.update_label(self.model.get_text())
