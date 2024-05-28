54003438e6e1fc5a1a6df55f5f4ff0cec11694a2
FazakasAbel <fazakas.abel@gmail.com>
2024-05-28 18:14:50 -0400

    file structure refactor

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = "./.env"
 load_dotenv(dotenv_path)
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
