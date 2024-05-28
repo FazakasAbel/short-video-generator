9cbb493e5fd6d1348d092ad1ec2eaa75cce9e6ba
FazakasAbel <fazakas.abel@gmail.com>
2024-05-28 18:15:10 -0400

    Move app.py to src

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from api import script, music, slideshow, user, project
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Register blueprints
+    app.register_blueprint(script.scripts_bp)
+    app.register_blueprint(slideshow.slideshows_bp)
+    app.register_blueprint(music.music_bp)
+    app.register_blueprint(user.users_bp)
+    app.register_blueprint(project.projects_bp)
+    return app
+
+app = create_app()
+
