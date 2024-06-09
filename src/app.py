f166f073a25639a0d4750433b6633ac1a4fdcc59
FazakasAbel <fazakas.abel@gmail.com>
2024-06-08 23:03:32 -0400

    add blueprints

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from api import script, music, slideshow, user, project
+from src.api import user, script, audio, project, render, image
 from flask import Flask
 import os
 
@@ -6,11 +6,12 @@ def create_app():
     app = Flask(__name__)
     
     # Register blueprints
-    app.register_blueprint(script.scripts_bp)
-    app.register_blueprint(slideshow.slideshows_bp)
-    app.register_blueprint(music.music_bp)
     app.register_blueprint(user.users_bp)
+    app.register_blueprint(script.scripts_bp)
+    app.register_blueprint(render.renders_bp)
+    app.register_blueprint(audio.audios_bp)
     app.register_blueprint(project.projects_bp)
+    app.register_blueprint(image.images_bp)
     return app
 
 app = create_app()
