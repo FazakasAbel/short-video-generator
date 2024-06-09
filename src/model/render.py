e2ce498ee92343c979ed09c457d688f87f5a030e
FazakasAbel <fazakas.abel@gmail.com>
2024-06-08 23:06:06 -0400

    create render model

--- src/model/render.py
+++ src/model/render.py
@@ -1,8 +1,25 @@
-from dataclasses import dataclass
-from typing import List
+class Render:
+    def __init__(self, id=None, file=None, filename=None):
+        self.id = id
+        self.file = file
+        self.filename = filename
 
-@dataclass
-class User:
-    username: str
-    email: str
-    project_ids: List[str]
+    def to_json(self, include_id=True):
+        result = {
+            'file': self.file,
+            'filename': self.filename
+        }
+        if include_id:
+            result['id'] = self.id
+        return result
+    
+    def equals(self, other):
+        return (self.file == other.file and
+                self.filename == other.filename)
+    
+    def merge_renders(self, new_render):
+        merged_render = Render(self.file, self.filename)
+        if new_render.filename:
+            merged_render.filename = new_render.filename
+
+        return merged_render
