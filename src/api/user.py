1e4c1ec643c4962f0bc267e53be8e01af18d8c17
FazakasAbel <fazakas.abel@gmail.com>
2024-06-08 23:05:05 -0400

    create user controller

--- src/api/user.py
+++ src/api/user.py
@@ -1,89 +1,84 @@
 # api/users.py
 from flask import Blueprint, request, jsonify
-from pymongo import MongoClient
-from bson import ObjectId
-from settings import MONGO_URI
-import os
+from service.user_service import UserService
+from src.api.exceptions.DocumentNotFound import DocumentNotFound
+from src.api.exceptions.NoUpdateDone import NoUpdateDone    
+from bson.objectid import ObjectId
+from src.model.user import User
+import logging
 
 users_bp = Blueprint('user', __name__)
 
-# Connect to MongoDB
-client = MongoClient(MONGO_URI)
-db = client.shortsdb
-users_collection = db.users
+user_service = UserService()
 
 @users_bp.route('/user', methods=['POST'])
-def create_user():
-    data = request.json
-    username = data.get('username')
-    email = data.get('email')
-    
-    if not username or not email:
-        return jsonify({"error": "Username and email are required"}), 400
-    
-    user = {
-        "username": username,
-        "email": email,
-        "projects": []
-    }
-    
-    inserted_user_result = users_collection.insert_one(user)
-    inserted_user = {
-        "_id": str(inserted_user_result.inserted_id),
-        "username": username,
-        "email": email
-    }
-    
-    return jsonify(inserted_user), 201
-
-@users_bp.route('/user/<user_id>', methods=['GET'])
-def get_user(user_id):
+def save_user():
     try:
-        user_id = ObjectId(user_id)
-    except:
-        return jsonify({"error": "Invalid user ID format"}), 400
-    
-    user = users_collection.find_one({"_id": user_id})
-    if user:
-        user['_id'] = str(user['_id'])
-        return jsonify(user), 200
-    else:
-        return jsonify({"error": "User not found"}), 404
-
-@users_bp.route('/user/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    try:
-        user_id = ObjectId(user_id)
-    except:
-        return jsonify({"error": "Invalid user ID format"}), 400
-    
-    user = users_collection.find_one({"_id": user_id})
-    if user:
         data = request.json
         username = data.get('username')
         email = data.get('email')
         
         if not username or not email:
             return jsonify({"error": "Username and email are required"}), 400
-
-        users_collection.update_one({"_id": user_id}, {"$set": {"username": username, "email": email}})
-        updated_user = users_collection.find_one({"_id": user_id})
-        updated_user['_id'] = str(updated_user['_id'])
         
-        return jsonify(updated_user), 200
-    else:
+        user_id = user_service.save_user(username, email)
+        return jsonify(user_id), 201
+    except Exception as e:
+        logging.exception(f"Failed to create user: {str(e)}")
+        return jsonify({"error": "Failed to create user"}), 500
+
+@users_bp.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        user_id = ObjectId(user_id)
+        user = user_service.get_user(user_id)
+        return user.to_json(), 200
+    
+    except DocumentNotFound as e:
         return jsonify({"error": "User not found"}), 404
+    
+    except Exception as e:
+        logging.exception(f"Failed to get user: {str(e)}")
+        return jsonify({"error": "Failed to get user"}), 500
+
+@users_bp.route('/user/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    try:
+        user_id = ObjectId(user_id)
+        data = request.json
+        username = data.get('username')
+        email = data.get('email')
+        project_ids = data.get('project_ids')
+        
+        if not username or not email or not project_ids:
+            return jsonify({"error": "Username, email and project ids are required"}), 400
+        
+        update_user = User(username=username, email=email, project_ids=project_ids)
+        user = user_service.update_user(user_id, update_user)
+        return user.to_json(), 200
+    
+    except DocumentNotFound as e:
+        return jsonify({"error": "User not found"}), 404
+    
+    except NoUpdateDone as e:
+        return jsonify({"error": "No update was done"}), 409
+    
+    except Exception as e:
+        logging.exception(f"Failed to update user: {str(e)}")
+        return jsonify({"error": "Failed to update user"}), 500
 
 @users_bp.route('/user/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     try:
         user_id = ObjectId(user_id)
-    except:
-        return jsonify({"error": "Invalid user ID format"}), 400
-    
-    user = users_collection.find_one({"_id": user_id})
-    if user:
-        users_collection.delete_one({"_id": user_id})
+        user_service.delete_user(user_id)
         return {}, 204
-    else:
+    
+    except DocumentNotFound as e:
         return jsonify({"error": "User not found"}), 404
+    
+    except Exception as e:
+        logging.exception(f"Failed to delete user: {str(e)}")
+        return jsonify({"error": "Failed to delete user"}), 500
+
+
