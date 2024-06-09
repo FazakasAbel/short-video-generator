acbb35f26c4be4daf3793d6eae4ed07b0ac7c3be
FazakasAbel <fazakas.abel@gmail.com>
2024-06-08 23:05:25 -0400

    create audio model

---
+++ src/model/audio.py
@@ -0,0 +1,25 @@
+class audio:
+    def __init__(self, id=None, file=None, filename=None):
+        self.id = id
+        self.file = file
+        self.filename = filename
+
+    def to_json(self, include_id=True):
+        result = {
+            'file': self.file,
+            'filename': self.filename
+        }
+        if include_id:
+            result['id'] = self.id
+        return result
+    
+    def equals(self, other):
+        return (self.file == other.file and
+                self.filename == other.filename)
+    
+    def merge_audios(self, new_audio):
+        merged_audio = audio(self.file, self.filename)
+        if new_audio.filename:
+            merged_audio.filename = new_audio.filename
+
+        return merged_audio
