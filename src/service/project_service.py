fa9a1b45cdb999acca8b103cfe3c0e829333432f
FazakasAbel <fazakas.abel@gmail.com>
2024-06-11 02:04:40 +0200

    add voiceover generation

--- src/service/project_service.py
+++ src/service/project_service.py
@@ -10,6 +10,8 @@ from src.model.project import Project
 from src.model.image_url import ImageUrl
 from src.service.movie_service import MovieService
 from src.service.render_service import RenderService
+from src.service.audio_service import AudioService
+from io import BytesIO
 import logging
 
 class ProjectService:
@@ -19,6 +21,7 @@ class ProjectService:
         self.script_service = ScriptService()
         self.movie_service = MovieService(30, (1024, 1792), 2)
         self.render_service = RenderService()
+        self.audio_service = AudioService()
 
     def save_project(self, user_id, title,  state, script_id=None, images=None, audio_id=None, render_id=None):
         project = Project(user_id=user_id, title=title, script_id=script_id, images=images, audio_id=audio_id, render_id=render_id, state=state)
@@ -86,6 +89,7 @@ class ProjectService:
 
         image_descriptions : list[ImageDescription] = script.get_image_descriptions()
 
+        #TODO use zip
         images_urls = [ImageUrl(url=self.image_service.generate_dalle_image_url(image_description.description), duration=image_description.duration) for image_description in image_descriptions]
         image_ids = []
         for image_url in images_urls:
@@ -138,6 +142,72 @@ class ProjectService:
 
         render_id = self.render_service.save_render(slideshow, f"{project.id}_slideshow.mp4")
 
+        if not render_id:
+            raise GenerationUnsuccessful("Failed to save render")
+
+        self.update_project_render(project_id, render_id)
+        return render_id
+    
+    def generate_project_voiceovers(self, project_id):
+        project : Project = self.repo.get_project(project_id)
+        if not project:
+            raise DocumentNotFound("Project not found")
+
+        script_id = project.script_id
+        if not script_id:
+            raise NoUpdateDone
+
+        script : Script = self.script_service.get_script(script_id)
+        if not script:
+            raise DocumentNotFound("Script not found")
+        
+        texts = script.get_text_descriptions()
+        if not texts or len(texts) == 0:
+            raise DocumentNotFound("Texts not found")
+        for text in texts:
+            logging.exception(text)
+        voiceovers = [self.audio_service.generate_voiceover(text) for text in texts]
+        if not voiceovers:
+            raise GenerationUnsuccessful("Failed to generate voiceovers")
+
+        voiceover_ids = [self.audio_service.save_audio(file=voiceover, filename=f"{i}_voiceover") for i, voiceover in enumerate(voiceovers)]
+
+        if not voiceover_ids:
+            raise GenerationUnsuccessful("Failed to save voiceovers")
+
+        self.update_project_voiceovers(project_id, voiceover_ids)
+        return voiceover_ids
+    
+    def update_project_voiceovers(self, project_id, voiceover_ids):
+        self.repo.update_project_voiceovers(project_id, voiceover_ids) 
+
+    def generate_project_narration(self, project_id):
+        project = self.repo.get_project(project_id)
+        if not project:
+            raise DocumentNotFound("Project not found")
+
+        voiceovers = project.voiceovers
+        if not voiceovers:
+            raise NoUpdateDone
+
+        voiceovers = [self.audio_service.get_audio(voiceover_id) for voiceover_id in voiceovers]
+        if not voiceovers:
+            raise DocumentNotFound("Voiceover not found")
+
+        render = self.render_service.get_render(project.render_id)
+        if not render:
+            raise DocumentNotFound("Render not found")
+        
+        script = self.script_service.get_script(project.script_id)
+        if not script:
+            raise DocumentNotFound("Script not found")
+
+        audios = zip(voiceovers, script.get_voiceover_durations())
+        narration = self.movie_service.add_narration_to_video(render, audios)
+        if not narration:
+            raise GenerationUnsuccessful("Failed to generate narration")
+
+        render_id = self.render_service.save_render(narration, f"{project.id}_narration.mp4")
         if not render_id:
             raise GenerationUnsuccessful("Failed to save render")
 
