4d3a972e6309892f0ba5a649ee82d414d08b0180
FazakasAbel <fazakas.abel@gmail.com>
2024-06-17 20:22:56 +0200

    add video download

--- src/service/render/render_service.py
+++ src/service/render/render_service.py
@@ -1,6 +1,7 @@
 from src.repo.render_repo import RenderRepository
 from src.api.exceptions.DocumentNotFound import DocumentNotFound
 from src.model.render import Render
+import requests
 
 class RenderService:
     def __init__(self):
@@ -23,4 +24,22 @@ class RenderService:
             raise DocumentNotFound
         
         self.repo.delete_render(render_id)
+
+    @staticmethod
+    def download_video(url):
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+
+            # Collect the video content as bytes
+            video_bytes = b""
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  # filter out keep-alive new chunks
+                    video_bytes += chunk
+
+            return video_bytes
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while downloading the video: {e}")
+            return None
 
