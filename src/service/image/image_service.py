632c978330aa883ca493e5e2f77a219344ff634a
FazakasAbel <fazakas.abel@gmail.com>
2024-07-03 01:49:51 +0200

    store image instead of url

--- src/service/image/image_service.py
+++ src/service/image/image_service.py
@@ -1,12 +1,13 @@
 import requests
 import os
 import logging as log
-from src.model.image_url import ImageUrl
+from src.model.image_url import Image
 from src.repo.image_repo import ImageUrlRepository
 from src.service.image.dalle_service import DalleService
 from src.api.exceptions.GenerationUnsuccessful import GenerationUnsuccessful
 from src.api.exceptions.DocumentNotFound import DocumentNotFound
 from src.api.exceptions.NoUpdateDone import NoUpdateDone
+from io import BytesIO
 
 class ImageService:
 
@@ -14,9 +15,10 @@ class ImageService:
         self.dalle_service = DalleService()
         self.image_repo = ImageUrlRepository()
     
-    def save_image_url(self, image_url, duration):
-        image_url = ImageUrl(url=image_url, duration=duration)
-        image_id = self.image_repo.save_image_url(image_url)
+    def save_image(self, url : str, duration : int) -> str:
+        content = self.download_image(url)
+        image = Image(file=content, duration=duration)
+        image_id = self.image_repo.save_image(image)
         if not image_id:
             log.warn("Failed to save image")
             raise Exception
@@ -24,45 +26,45 @@ class ImageService:
         log.info("Image saved successfully")
         return image_id
 
-    def get_image_url(self, image_id):
-        image_url = self.image_repo.get_image_url(image_id)
-        if not image_url:
+    def get_image(self, image_id : str) -> Image:
+        image = self.image_repo.get_image(image_id)
+        if not image:
             log.warn("Failed to get image")
             raise DocumentNotFound("Image not found")
 
         log.info("Image fetched successfully")
-        return image_url
+        return image
 
-    def update_image_url(self, image_id, updated_data : ImageUrl):
-        image_url = self.image_repo.get_image_url(image_id)
-        if not image_url:
+    def update_image(self, image_id : str, updated_data : Image) -> None:
+        image = self.image_repo.get_image(image_id)
+        if not image:
             log.warn("Failed to get image")
             raise DocumentNotFound
 
-        merged_image : ImageUrl = image_url.merge_images(updated_data)
-        if image_url.equals(merged_image):
+        merged_image : Image = image.merge_images(updated_data)
+        if image.equals(merged_image):
             log.warn("No update done")
             raise NoUpdateDone
 
-        self.image_repo.update_image_url(image_id, merged_image)
+        self.image_repo.update_image(image_id, merged_image)
 
-    def delete_image_url(self, image_id):
-        deleted = self.image_repo.delete_image_url(image_id)
+    def delete_image(self, image_id : str) -> None:
+        deleted = self.image_repo.delete_image(image_id)
         
         if not deleted:
             log.warn("Failed to delete image")
             raise DocumentNotFound    
 
-    def download_image(self, image_url):
+    def download_image(self, image_url : str) -> BytesIO:
         response = requests.get(image_url)
         if response.status_code != 200:
             log.warn("Failed to download image", response.status_code)
             raise Exception
 
         log.info("Image downloaded successfully")
-        return response.content
+        return BytesIO(response.content)
  
-    def generate_dalle_image_url(self, prompt, size="1024x1792", quality="standard"):
+    def generate_dalle_image(self, prompt : str, size : str = "1024x1792", quality : str = "standard") -> Image:
         
         image_url = self.dalle_service.generate_dalle_image(prompt, size, quality)
         if not image_url:
