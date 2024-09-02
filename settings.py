a5ca65f2c111cf26d743cba9ca6960aa62eb15b4
FazakasAbel <fazakas.abel@gmail.com>
2024-09-02 20:52:49 +0200

    optional local .env

--- settings.py
+++ settings.py
@@ -2,8 +2,15 @@
 import os
 from dotenv import load_dotenv
 
+# Define the path to the .env file
 dotenv_path = "./.env"
-load_dotenv(dotenv_path)
+
+# Check if the .env file exists before loading it
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+else:
+    print(f"Warning: {dotenv_path} file not found. Proceeding without loading environment variables from local source.")
+
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 VOICE_ID = os.environ.get("VOICE_ID")
